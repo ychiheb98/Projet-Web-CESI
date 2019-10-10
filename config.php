827377a9c912012f6f6e86df1b9c71f309e7608e
Imad <Im.benbadda@gmail.com>
2019-10-10 16:22:40 +0200

    recup gitignore

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $SETTINGS["mysql_database"] = 'foodies';
 $SETTINGS["USERS"] = 'php_users_login'; // this is the default table name that we used
 
 /* Connect to MySQL */
-$connection = mysqli_connect("localhost","root","", $SETTINGS["mysql_database"]) or die ('Unable to connect to MySQL server.<br ><br >Please make sure your MySQL login details are correct.');
+$connection = mysqli_connect("localhost:8889","root","root", $SETTINGS["mysql_database"]) or die ('Unable to connect to MySQL server.<br ><br >Please make sure your MySQL login details are correct.');
 ?>
