1e30e007b8aec2d163e245ef97a069efce582206
BERTRAND Alexis <bertrandalexis33@gmail.com>
2019-10-11 15:56:41 +0200

    index.php do

--- header.php
+++ header.php
@@ -2,19 +2,31 @@
 <html>
 <head>
 <title><?php echo "$title" ?></title>
+
+   <!-- Stylesheets -->
+   <link rel="stylesheet" href="css/plugins.css">
+	<link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="css/custom.css">
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   
+ <!-- Modernizer js -->
+ <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+
 </head>
 <body>
-<FONT size="4" color="white">
-<NAV align="right">
-<A HREF="index.php">Home</A>&nbsp&nbsp&nbsp
-<A HREF="help.php">Help</A>&nbsp&nbsp&nbsp
+<FONT size="4">
+<nav class=" navbar-right navbar-light" style="background-color:#c7c7c7;">
+  <!-- Navbar content -->
+
+<A class="navbar-brand" HREF="index.php" style="padding-left:25px">Home</A>&nbsp&nbsp&nbsp
+<A class="navbar-brand" HREF="help.php">Help</A>&nbsp&nbsp&nbsp
 <?php  
 session_start();
 if(isset($_SESSION['user_info']))
 	echo 'Welcome <A HREF="login.php"> '.$_SESSION['user_info'].'</a>';
 else
-	echo '<A HREF="register.php">Login/Register</A>';
-?>
-</FONT></NAV>
+	echo '<A class="navbar-brand" HREF="register.php">Login/Register</A>';
+?> 
+</FONT></nav>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,35 +1,43 @@
-<HTML>
+<HTML class="no-js" lang="zxx">
 <HEAD>
-<TITLE>Welcome to 24X7 Foodies.com!</TITLE>
-<style type="text/css">
-@import url(style.css);
-   a:link {color: #ffffff}
-   a:visited {color: #ffffff}
-   a:hover {color: #ffffff}
-   a:active {color: #ffffff}
-</style>
+<meta charset="utf-8">
+<TITLE>green therapie</TITLE>
+<!-- Favicons -->
+   <link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/icon.png">
+
+   <!-- Stylesheets -->
+   <link rel="stylesheet" href="css/plugins.css">
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="css/custom.css">
+   
+ <!-- Modernizer js -->
+ <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+
 </HEAD>
-<BODY background="background1.jpg">
+<BODY background="background5.jpg">
 <?php include("header.php"); ?>
-<FONT size="5" color="white">
-<SECTION align="center"><A HREF="index.php"><IMG SRC="logo.png" alt="Home" id="logo"></IMG></A></SECTION>
+<FONT size="5">
+<SECTION align="center"><A HREF="index.php"><IMG SRC="img/LogoMakr_8lqnaO.png" alt="Home" id="logo" style="height: 200px;"></IMG></A></SECTION>
 <SECTION>
-<MAIN>
-<B><P>We are available only at the following regions.<BR> Please select any one:</P></B></FONT>
-<SECTION align="center"><IMG src="clickhere.gif" width="100" height="50"></IMG></SECTION>
-<TABLE >
-<TR><TD><FONT size="6"  color="white">
-<A HREF="order.php" style="text-decoration: none">1)Kandivali West</A></FONT></TD></TR>
-<TR><TD><FONT size="6"  color="white">
-<A HREF="order.php" style="text-decoration: none">2)Kandivali East</A></FONT></TD></TR>
-<TR><TD><FONT size="6"  color="white">
-<A HREF="order.php" style="text-decoration: none">3)Borivali West</A></FONT></TD></TR>
-<TR><TD><FONT size="6"  color="white">
-<A HREF="order.php" style="text-decoration: none">4)Borivali East</A></FONT></TD></TR>
-</TABLE>
+<MAIN><BR>
+<B style="text-align:center"><P>We are available only at the following regions.<BR> Please select any one:</P></B></FONT>
+<!-- <SECTION align="center"><IMG src="clickhere.gif" width="100" height="50"></IMG></SECTION> -->
+<div class= "container-fluid">
+<div class= "row">
+<div class="col-md-3">
+</div>
+<div class="col-md-8">
+        <input type="text" class="toInput" placeholder="Type Place, City.Division" >
+        <button type="button" class="btn-lg btn-outline-success my-2 my-sm-0" style="height: 60px;">Success</button>
+</div>
+</div>
+</div>
+</div>
 </SECTION>
-</MAIN><BR><HR width="1000">
-<FOOTER >
+</MAIN><BR>
+<FOOTER style="bottom: 0;position: fixed;" >
 <FONT size="2" color="white">
 By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. &copy 2015-2016 - 24x7 Foodies Media Pvt Ltd. All rights reserved.</FONT>
 </FOOTER>
